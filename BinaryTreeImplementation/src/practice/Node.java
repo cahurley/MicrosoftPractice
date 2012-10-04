874283f3ef24b9014d85d47a509732b752ceec59
Caitlin Hurley <caitlin.jhurley@gmail.com>
2012-10-03 21:34:18 -0600

    completed binary tree node

--- BinaryTreeImplementation/src/practice/Node.java
+++ BinaryTreeImplementation/src/practice/Node.java
@@ -2,5 +2,49 @@ package practice;
 
 public class Node<E> 
 {
-
+	private E data;
+	private Node<E> leftChild;
+	private Node<E> rightChild;
+	
+	public Node(E data, Node<E> leftChild, Node<E> rightChild)
+	{
+		this.data = data; 
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
+	
+	public boolean isLeaf()
+	{
+		return (leftChild == null && rightChild == null);
+	}
+	
+	public E getData()
+	{
+		return this.data;
+	}
+	
+	public void setData(E data)
+	{
+		this.data = data;
+	}
+	
+	public Node<E> getLeftChild()
+	{
+		return this.leftChild;
+	}
+	
+	public void setLeftChild(Node<E> leftChild)
+	{
+		this.leftChild = leftChild;
+	}
+	
+	public Node<E> getRightChild()
+	{
+		return this.rightChild;
+	}
+	
+	public void setRightChild(Node<E> rightChild)
+	{
+		this.rightChild = rightChild;
+	}
 }
--- BinaryTreeImplementation/src/test/TestNode.java
+++ BinaryTreeImplementation/src/test/TestNode.java
@@ -1,6 +1,49 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import practice.Node;
+
 public class TestNode 
 {
-
+	Node<Integer> rootNode;
+	Node<Integer> nodeLeft;
+	Node<Integer> nodeRight;
+	
+	@Before
+	public void createNode()
+	{
+		nodeLeft = new Node<Integer>(10, null, null);
+		nodeRight = new Node<Integer>(5, null, null);
+		rootNode = new Node<Integer>(15, nodeLeft, nodeRight);
+	}
+	
+	@Test
+	public void testConstructor()
+	{
+		assertEquals(10, nodeLeft.getData(), 0);
+		assertEquals(null, nodeLeft.getLeftChild());
+		assertEquals(null, nodeLeft.getRightChild());
+		
+		assertEquals(5, nodeRight.getData(), 0);
+		assertEquals(null, nodeRight.getLeftChild());
+		assertEquals(null, nodeRight.getRightChild());
+		
+		assertEquals(15, rootNode.getData(), 0);
+		assertEquals(nodeLeft, rootNode.getLeftChild());
+		assertEquals(10, rootNode.getLeftChild().getData(), 0);
+		assertEquals(nodeRight, rootNode.getRightChild());
+		assertEquals(5, rootNode.getRightChild().getData(), 0);
+	}
+	
+	@Test
+	public void testIsLeaf()
+	{
+		assertTrue(nodeLeft.isLeaf());
+		assertTrue(nodeRight.isLeaf());
+		assertFalse(rootNode.isLeaf());
+	}
 }
